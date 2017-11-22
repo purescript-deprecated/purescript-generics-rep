6426536121273b5d283536a7b9bad87cefe36aa9
Van Dalen <rvandal@emea.corpdir.net>
2017-11-22 08:17:47 +0200

    Simplify Data.Generic.Rep.Show implementation.

--- src/Data/Generic/Rep/Show.purs
+++ src/Data/Generic/Rep/Show.purs
@@ -16,55 +16,44 @@ import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 class GenericShow a where
   genericShow' :: a -> String
 
-class GenericShowArgs a where
-  genericShowArgs :: a -> Array String
-
-class GenericShowFields a where
-  genericShowFields :: a -> Array String
-
 instance genericShowNoConstructors :: GenericShow NoConstructors where
   genericShow' a = genericShow' a
 
-instance genericShowArgsNoArguments :: GenericShowArgs NoArguments where
-  genericShowArgs _ = []
+instance genericShowNoArgs :: GenericShow NoArguments where
+  genericShow' _ = ""
 
 instance genericShowSum :: (GenericShow a, GenericShow b) => GenericShow (Sum a b) where
-  genericShow' (Inl a) = genericShow' a
-  genericShow' (Inr b) = genericShow' b
+  genericShow' (Inl l) = genericShow' l
+  genericShow' (Inr r) = genericShow' r
 
-instance genericShowArgsProduct
-    :: (GenericShowArgs a, GenericShowArgs b)
-    => GenericShowArgs (Product a b) where
-  genericShowArgs (Product a b) = genericShowArgs a <> genericShowArgs b
-
-instance genericShowFieldsProduct
-    :: (GenericShowFields a, GenericShowFields b)
-    => GenericShowFields (Product a b) where
-  genericShowFields (Product a b) = genericShowFields a <> genericShowFields b
+instance genericShowProduct
+  :: (GenericShow a, GenericShow b)
+  => GenericShow (Product a b)
+  where
+    genericShow' (Product l r) = (genericShow' l) <> ", " <> (genericShow' r)
 
 instance genericShowConstructor
-  :: (GenericShowArgs a, IsSymbol name)
-  => GenericShow (Constructor name a) where
-  genericShow' (Constructor a) =
-      case genericShowArgs a of
-        [] -> ctor
-        args -> "(" <> intercalate " " ([ctor] <> args) <> ")"
-    where
-      ctor :: String
-      ctor = reflectSymbol (SProxy :: SProxy name)
+  :: (GenericShow a, IsSymbol name)
+  => GenericShow (Constructor name a)
+  where
+    genericShow' (Constructor a) =
+      case genericShow' a of
+        ""   -> ctor
+        args -> "(" <> (ctor <> args) <> ")"
+      where
+        ctor :: String
+        ctor = reflectSymbol (SProxy :: SProxy name)
 
-instance genericShowArgsArgument :: Show a => GenericShowArgs (Argument a) where
-  genericShowArgs (Argument a) = [show a]
+instance genericShowArgument :: Show a => GenericShow (Argument a) where
+  genericShow' (Argument a) = show a
 
-instance genericShowArgsRec :: GenericShowFields a => GenericShowArgs (Rec a) where
-  genericShowArgs (Rec a) = ["{ " <> intercalate ", " (genericShowFields a) <> " }"]
+instance genericShowRec :: (GenericShow a) => GenericShow (Rec a) where
+  genericShow' (Rec a) = "{ " <> (genericShow' a) <> " }"
 
-instance genericShowFieldsField
-    :: (Show a, IsSymbol name)
-    => GenericShowFields (Field name a) where
-  genericShowFields (Field a) =
-    [reflectSymbol (SProxy :: SProxy name) <> ": " <> show a]
+instance genericShowFieldsField :: (Show a, IsSymbol name) => GenericShow (Field name a) where
+  genericShow' (Field a) = reflectSymbol (SProxy :: SProxy name) <> ": " <> show a
 
 -- | A `Generic` implementation of the `show` member from the `Show` type class.
 genericShow :: forall a rep. Generic a rep => GenericShow rep => a -> String
-genericShow x = genericShow' (from x)
+genericShow o = genericShow' (from o)
+
